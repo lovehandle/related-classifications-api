5a2991adc9dc19053b4c45711b8fd78448e54139
Ryan Closner <ryan@ryanclosner.com>
2014-01-16 15:19:44 -0800

    Create related classification property

--- datasets/BUSINESS_LICENSING/build.js
+++ datasets/BUSINESS_LICENSING/build.js
@@ -1,9 +1,9 @@
 var buildIndex  = require( process.cwd() + '/lib/build_index' );
 
-var OBJECTS     = require( './dataset' ).items;
-var INDEX_PATH  = './datasets/naics_2012/index.json';
+var OBJECTS     = require( './dataset' );
+var INDEX_PATH  = './datasets/business_licensing/index.json';
 var ID_PROPERTY = 'code';
-var PROPERTIES  = [ 'code', 'title', 'index', 'examples', 'description' ];
+var PROPERTIES  = [ 'code', 'name' ];
 
 module.exports = function () {
   buildIndex(OBJECTS, PROPERTIES, ID_PROPERTY, INDEX_PATH);
--- datasets/naics_2007/build.js
+++ datasets/naics_2007/build.js
@@ -1,44 +1,10 @@
-var _  = require('underscore');
-var fs = require('fs');
+var buildIndex  = require( process.cwd() + '/lib/build_index' );
 
-var tokenize = require( process.cwd() + '/lib/tokenize' );
-var dataset  = require('./dataset');
-
-var PATH   = './datasets/naics_2007/index.json';
-var FIELDS = [ 'code', 'title', 'index', 'examples', 'description' ];
+var OBJECTS     = require( './dataset' ).items;
+var INDEX_PATH  = './datasets/naics_2007/index.json';
+var ID_PROPERTY = 'code';
+var PROPERTIES  = [ 'code', 'title', 'index', 'examples', 'description' ];
 
 module.exports = function () {
-  var index = {};
-
-  _.each(dataset.items, function (item) {
-    var code = item.code;
-
-    _.each(FIELDS, function (field) {
-      var score = FIELDS.length - FIELDS.indexOf(field);
-      var text  = item[field];
-
-      text = _.isArray(text) ? text : [ text ];
-
-      _.each(text, function (t) {
-        var terms = tokenize(t);
-
-        _.each(terms, function (term) {
-          index[term] = index[term] || {};
-          index[term][code] = index[term][code] || 0;
-          index[term][code] += score;
-        })
-
-      })
-
-    }) 
-  });
-
-  fs.writeFile(PATH, JSON.stringify(index), function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Index successfully created.') 
-    }
-  });
-
+  buildIndex(OBJECTS, PROPERTIES, ID_PROPERTY, INDEX_PATH);
 }
