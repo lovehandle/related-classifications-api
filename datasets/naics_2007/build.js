01dbf4456d45386cdaf05ca5acf0f228a51c66e8
Ryan Closner <ryan@ryanclosner.com>
2014-01-15 16:57:32 -0800

    Initial commit

---
+++ datasets/naics_2007/build.js
@@ -0,0 +1,44 @@
+var _  = require('underscore');
+var fs = require('fs');
+
+var tokenize = require( process.cwd() + '/lib/tokenize' );
+var dataset  = require('./dataset');
+
+var PATH   = './datasets/naics_2007/index.json';
+var FIELDS = [ 'code', 'title', 'index', 'examples', 'description' ];
+
+module.exports = function () {
+  var index = {};
+
+  _.each(dataset.items, function (item) {
+    var code = item.code;
+
+    _.each(FIELDS, function (field) {
+      var score = FIELDS.length - FIELDS.indexOf(field);
+      var text  = item[field];
+
+      text = _.isArray(text) ? text : [ text ];
+
+      _.each(text, function (t) {
+        var terms = tokenize(t);
+
+        _.each(terms, function (term) {
+          index[term] = index[term] || {};
+          index[term][code] = index[term][code] || 0;
+          index[term][code] += score;
+        })
+
+      })
+
+    }) 
+  });
+
+  fs.writeFile(PATH, JSON.stringify(index), function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Index successfully created.') 
+    }
+  });
+
+}
